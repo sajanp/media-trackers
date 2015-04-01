62633254107987fdb5e5df08232d2c952492a5e1
Sajan Parikh <sajan@parikh.io>
2015-04-01 03:49:32 -0500

    store movie purchase

--- app/Http/Controllers/Title/MoviePurchaseController.php
+++ app/Http/Controllers/Title/MoviePurchaseController.php
@@ -2,20 +2,58 @@
 
 use App\Http\Controllers\Controller;
 use App\Entities\Title\TitleInterface;
+use App\Entities\Purchase\PurchaseInterface;
+use App\Entities\Ultraviolet\UltravioletInterface;
 use App\Entities\Title\EloquentTitle as Title;
 use Illuminate\Contracts\Support\MessageBag;
 use Illuminate\Http\Request;
 
 class MoviePurchaseController extends Controller {
 
+	private $purchases;
+	private $titles;
+
+	public function __construct(PurchaseInterface $purchases, TitleInterface $titles)
+	{
+		$this->purchases = $purchases;
+		$this->titles = $titles;
+	}
+
 	public function create($movieId)
 	{
 		return view('title.movie.purchase.create');
 	}
 
-	public function store(Request $request, $movieId)
+	public function store(UltravioletInterface $ultraviolet, Request $request, $movieId)
 	{
-		dd($request->all());
+		try
+		{
+			$purchase = $this->purchases->create([
+				'closed' => true,
+				'retailer_id' => $request->input('retailer_id'),
+				'amount' => $request->input('amount'),
+				'note' => $request->input('note'),
+				'purchased_on' => $request->input('purchased_on')
+			]);
+
+			$movie = $this->titles->getMovieById($movieId);
+
+			foreach($request->input('formats', array()) as $format)
+			{
+				$purchase->movies()->save($movie, ['format_id' => $format, 'edition' => $request->input('edition', '')]);
+			}
+
+			if ($request->input('isUltraviolet', false))
+			{
+				$ultraviolet->create($movie, $purchase, $request->input('edition'));
+			}
+
+			return redirect()->route('movie.show', $movie->id);
+		}
+		catch(Exception $e)
+		{
+			dd($e->getMessage());
+		}
 	}
 
 }
