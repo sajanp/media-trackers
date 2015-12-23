ef42494ece1d29c6131f76f29fda302f9dcc994e
Sajan Parikh <sajan@parikh.io>
2015-12-23 17:30:08 -0600

    add closed column to rentals table

---
+++ database/migrations/2015_12_23_232654_add_closed_to_rentals.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddClosedToRentals extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('rentals', function (Blueprint $table) {
+            $table->boolean('closed')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('rentals', function (Blueprint $table) {
+            $table->dropColumn('closed');
+        });
+    }
+}
